23603efb9b7b9b95a45333b2f10408d94cc5aa29
shivam sangal <shivamsangal@shivams-MacBook-Air.local>
2025-06-03 17:37:08 +0530

    indiconnect api

--- src/Services/AuthServices.tsx
+++ src/Services/AuthServices.tsx
@@ -1,10 +1,13 @@
 import api from "./Axios/api";
 export async function generateOTP(aadharNo: string) {
   try {
-    const response = await api.post("/users/generate-otp", { aadharNo },
-    {
-      withCredentials: true,
-    });
+    const response = await api.post(
+      "/api/aadhaar-otp",
+      { id_number: aadharNo, document_type: "Aadhaar OTP Validation" },
+      {
+        withCredentials: true,
+      }
+    );
     return response.data;
   } catch (error) {
     console.error(error);
@@ -12,16 +15,20 @@ export async function generateOTP(aadharNo: string) {
   }
 }
 
-export async function validateUser(email: string, otp: string ,platform:string,
-  identifier:string) {
+export async function validateUser(
+  email: string,
+  otp: string,
+  platform: string,
+  identifier: string
+) {
   try {
     const response = await api.post(
       "/users/login",
       {
         email: email,
         otp: otp,
-        platform : platform,
-        identifier : identifier
+        platform: platform,
+        identifier: identifier,
       },
       {
         withCredentials: true,
@@ -37,24 +44,26 @@ export async function validateUser(email: string, otp: string ,platform:string,
 export async function validateOTP(otp: string, reqId: string) {
   try {
     const response = await api.post(
-      "/users/validate-otp",
+      "/api/aadhaar-submit-otp",
       {
         otp: otp,
-        reqID: reqId,
+        client_id: reqId,
       },
       {
         withCredentials: true,
       }
     );
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
-    throw error?.response.data.message;
+    throw (
+      error?.response?.data?.message ||
+      "An error occurred while validating OTP."
+    );
   }
 }
 
-
-export async function validatePanCard(panNumber:any){
+export async function validatePanCard(panNumber: any) {
   try {
     const response = await api.post(
       "/users/validatePancard",
@@ -72,7 +81,7 @@ export async function validatePanCard(panNumber:any){
   }
 }
 
-export async function validateVoterID(VoterIdNumber:any){
+export async function validateVoterID(VoterIdNumber: any) {
   try {
     const response = await api.post(
       "/users/validateVoterid",
